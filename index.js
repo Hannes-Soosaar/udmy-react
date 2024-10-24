e12e81249ed701261dfc4c90de8c29ee96de7d05
hannes <hannes.study@gmail.com>
2024-10-24 10:33:13 +0300

    connected DB and oAuth

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require(`express`);
+const mongoose= require(`mongoose`);
+const keys= require('./config/keys.js');
+require('./services/passport.js');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get(`/`,(req,res)=> {
-res.send({hi: 'there'});
-});
+require(`./routes/authRoutes.js`)(app);
 
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
