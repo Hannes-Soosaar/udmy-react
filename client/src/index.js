8f3619e2b282635f38b5b2c0c2c816bcb42ed3a3
hannes <hannes.study@gmail.com>
2024-10-29 10:37:17 +0200

    WIP  axios and thunk setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,18 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux'; // the Provide tag is imported from here
+import { Provider } from 'react-redux'; // the Provide tag is imported from here
 import { createStore, applyMiddleware } from 'redux';
 // import ReactDOM from 'react-dom'; // used in the tutorial 
+import {thunk} from 'redux-thunk';
+
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
-const store = createStore( ()=> [],{}, applyMiddleware());
 const el = document.getElementById("root");
-const root= ReactDOM.createRoot(el);
-root.render(<Provider store={store}><App/></Provider>) // Provider is a pre-made components, that passes store to all Ract component within the Provider tag is there are updated to the store
+const root = ReactDOM.createRoot(el);
+root.render(<Provider store={store}><App /></Provider>) // Provider is a pre-made components, that passes store to all Ract component within the Provider tag is there are updated to the store
 
 // ReactDOM.render(<App/>, document.querySelector('#root')); // used in the tutorial
