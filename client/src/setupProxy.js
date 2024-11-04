2816de85610f124f95a3f08ff5528d43182b9269
hannes <hannes.study@gmail.com>
2024-11-04 15:23:08 +0200

    WIP

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     console.log("setting up proxy");
     app.use(
-        ["/api", "/auth/google"],
         createProxyMiddleware({
             target: "http://localhost:5000",
             changeOrigin: true,
             logLevel: "debug", // Logs proxy activity
+            pathFilter: ["/api", "/auth/google"]
         })
     );
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,23 +2,23 @@ const passport = require(`passport`);
 
 module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-    
-    app.get('/auth/google/callback',
-         passport.authenticate('google'), (req,res) => {
-            res.redirect('/surveys');
-         });
 
-    app.get(`/api/logout`,(req,res) =>{
+    app.get('/auth/google/callback',
+        passport.authenticate('google'), (req, res) => {
+            res.redirect('/surveys');
+        });
+
+    app.get(`/api/logout`, (req, res) => {
         console.log("LOG OUT!")
         req.logout();
         res.redirect("/")
     });
-    app.get(`/api/current_user`,(req,res)=>{
+    app.get(`/api/current_user`, (req, res) => {
         console.log(req.user);
         res.send(req.user);
     });
 
-    app.get("/api/test", (req, res) =>{
+    app.get("/api/test", (req, res) => {
         res.send("some random string");
         console.log("we are Getting some info from TEST")
     });
